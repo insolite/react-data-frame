fe2bfd3a10635d035e88c6e652c8ecbc187be744
Oleg Krasnikov <a.insolite@gmail.com>
2017-09-17 12:52:57 +0300

    Fix selected row style, add favicon

--- src/rich/fragments/Row.jsx
+++ src/rich/fragments/Row.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Row = props => {
     const { children, data, index, selected, ...rowProps } = props;
     return (
-        <div className={`react-frame-table--row ${selected ? 'selected' : ''}`} {...rowProps}>
+        <div className={`react-frame-table--row ${selected ? 'react-frame-table--selected' : ''}`} {...rowProps}>
             {children}
         </div>
     );
