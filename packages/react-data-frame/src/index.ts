2e2bfc0e130e985f61fd12d6a9dd4ed4f14ab64e
Oleg Krasnikov <a.insolite@gmail.com>
2022-08-23 12:03:14 +0300

    Fix start command

--- packages/react-data-frame/src/index.ts
+++ packages/react-data-frame/src/index.ts
@@ -1,4 +1,8 @@
 export { default as useDataFrame } from './useDataFrame';
-export type { DataFrameOptions, DataFrameState } from './useDataFrame';
+export type {
+  DataFrameOptions,
+  DataFrameState,
+  SetFrameIndex,
+} from './useDataFrame';
 export { default as VirtualScroll } from './VirtualScroll';
 export type { VirtualScrollProps } from './VirtualScroll';
